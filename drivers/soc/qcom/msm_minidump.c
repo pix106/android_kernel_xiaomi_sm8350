c156212ebef35e96639e3ca3f15618d5dd06247b
Kristof Petho <kristof.petho@gmail.com>
2021-06-09 08:58:38 +0200

    treewide: Fix compilation after disabling -fno-builtin
    Change-Id: I634d8f8351d2bf10236ac57187dd99b9b8e10842

--- drivers/soc/qcom/msm_minidump.c
+++ drivers/soc/qcom/msm_minidump.c
@@ -448,6 +448,7 @@ static int msm_minidump_add_header(void)
 	struct elf_phdr *phdr;
 	unsigned int strtbl_off, elfh_size, phdr_off;
 	char *banner;
+	size_t linux_banner_len = strlen(linux_banner);
 
 	/* Header buffer contains:
 	 * elf header, MAX_NUM_ENTRIES+4 of section and program elf headers,
